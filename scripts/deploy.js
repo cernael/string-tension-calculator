9682531931688cbceaa8a9f9d5db54835b951870
Aaron Abramov <aaron@abramov.io>
2018-12-15 18:39:51 -0800

    add deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+const os = require('os');
+const path = require('path');
+const sh = require('shelljs');
+const chalk = require('chalk');
+
+sh.config.verbose = true;
+
+const TMPDIR = os.tmpdir();
+const DEPLOY_BRANCH = 'deploy-prod';
+
+const buildTar = path.resolve(TMPDIR, 'string-tension-calculator.tar');
+
+console.log(chalk.cyan(buildTar));
+
+sh.exec('yarn build');
+sh.exec(`tar cvf ${buildTar} ./build`);
+sh.exec('rm -rf ./build');
+
+sh.exec('git checkout deploy-prod');
+
+sh.exec('rm -rf ./build');
+sh.mv(buildTar, './build.tar');
+sh.exec('tar xvf ./build.tar');
+sh.rm('./build.tar');
+sh.exec('git status');
+sh.exec('git add ./build/');
+sh.exec(`git commit -m "build ${new Date().toISOString()} [ci skip]"`);
+sh.exec(`git push -f origin ${DEPLOY_BRANCH}`);
