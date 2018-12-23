54d224cb1fbac75fd1eb921388038f6c6f62361d
Aaron Abramov <aaron@abramov.io>
2018-12-23 14:58:44 -0800

    event logging

--- src/data/ga_logger.js
+++ src/data/ga_logger.js
@@ -7,9 +7,10 @@ import type {Action} from '../types.js';
 export const log = (action: Action) => {
   try {
     // $FlowFixMe ga global
-    ga('send', 'event', {
+    ga('send', {
+      hitType: 'event',
       eventCategory: 'redux_action',
-      evertAction: action.type,
+      eventAction: action.type,
     });
   } catch (e) {
     console.log('failed to log ga event');
