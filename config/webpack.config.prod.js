7bf33ec020bfacfad3f3b1e3d9ae851f86d82657
Aaron Abramov <aaron@abramov.io>
2019-03-04 20:08:40 -0500

    es6-shim

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -246,7 +246,7 @@ module.exports = {
         removeEmptyAttributes: true,
         removeStyleLinkTypeAttributes: true,
         keepClosingSlash: true,
-        minifyJS: true,
+        minifyJS: false,
         minifyCSS: true,
         minifyURLs: true,
       },
@@ -308,7 +308,7 @@ module.exports = {
         }
         console.log(message);
       },
-      minify: true,
+      minify: false,
       // For unknown URLs, fallback to the index page
       navigateFallback: publicUrl + '/index.html',
       // Ignores URLs starting from /__ (useful for Firebase):
--- src/shim.js
+++ src/shim.js
@@ -1,5 +1,1 @@
-import includes from 'array-includes';
-import find from 'array.prototype.find';
-
-includes.shim();
-find.shim();
+import 'es6-shim';
