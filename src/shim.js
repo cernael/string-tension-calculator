e78060c66b0945115f1b516666f8660aeacdc47c
Aaron Abramov <aaron@abramov.io>
2019-03-04 20:16:05 -0500

    Array.prototype.fill

--- src/shim.js
+++ src/shim.js
@@ -1,1 +1,8 @@
-import 'es6-shim';
+import includes from 'array-includes';
+import find from 'array.prototype.find';
+import fill from 'array.prototype.fill';
+
+includes.shim();
+find.shim();
+// eslint-disable-next-line
+fill;
