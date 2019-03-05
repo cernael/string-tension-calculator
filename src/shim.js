d6c96dccb3d3486d7ee5a4922c0ea82adfc98d6b
Aaron Abramov <aaron@abramov.io>
2019-03-04 20:20:36 -0500

    Object.entries

--- src/shim.js
+++ src/shim.js
@@ -1,8 +1,10 @@
 import includes from 'array-includes';
 import find from 'array.prototype.find';
 import fill from 'array.prototype.fill';
+import entries from 'object.entries';
 
 includes.shim();
 find.shim();
+entries.shim();
 // eslint-disable-next-line
 fill;
