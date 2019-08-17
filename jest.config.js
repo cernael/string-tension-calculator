5db0318f962153b0f54627179f859f8acf7780f1
Aaron Abramov <aaron@abramov.io>
2019-08-17 13:11:21 -0400

    Monorepo

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    rootDir: __dirname,
+    displayName: "website",
+    collectCoverageFrom: [
+        "src/**/*.{js,jsx,mjs}"
+    ],
+    setupFiles: [
+        "<rootDir>/packages/website/config/polyfills.js"
+    ],
+    testMatch: [
+        "<rootDir>/**/__tests__/**/*.{js,jsx,mjs}",
+    ],
+    testEnvironment: "node",
+    testURL: "http://localhost",
+    transform: {
+        "^.+\\.(js|jsx|mjs)$": "<rootDir>/node_modules/babel-jest",
+        "^.+\\.css$": "<rootDir>/packages/website/config/jest/cssTransform.js",
+        "^(?!.*\\.(js|jsx|mjs|css|json)$)": "<rootDir>/packages/website/config/jest/fileTransform.js"
+    },
+    transformIgnorePatterns: [
+        "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$"
+    ],
+    moduleNameMapper: {
+        "^react-native$": "react-native-web"
+    },
+    moduleFileExtensions: [
+        "web.js",
+        "js",
+        "json",
+        "web.jsx",
+        "jsx",
+        "node",
+        "mjs"
+    ]
+};
